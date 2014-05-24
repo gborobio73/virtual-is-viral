d6ad71bc830b4594bc912baf047c69677b49bb08
gborobio73 <gborobio@gmail.com>
2014-05-24 22:47:25 +0300

    heroku

---
+++ target/virtual-is-viral-1.0-SNAPSHOT/services.js
@@ -0,0 +1,93 @@
+angular.module('vivServices').factory('services', function($http, $http) {
+    var services = new Object();
+
+    services.getAllWorks = function() {
+             //return the promise directly.
+             return $http.get('/rest/works/all')
+                       .then(function(result) {
+                            //resolve the promise as the data
+                            return result.data;
+                        });
+        }
+
+    services.getUserWorks = function(user) {
+             //return the promise directly.
+             return $http.get('/rest/works/user/'+user)
+                       .then(function(result) {
+                            //resolve the promise as the data
+                            return result.data;
+                        });
+        }
+    services.getWork = function(workId) {
+             //return the promise directly.
+             return $http.get('/rest/works/work/'+workId)
+                       .then(function(result) {
+                            //resolve the promise as the data
+                            return result.data;
+                        });
+        }
+    services.getUnreadNotificationsAmount = function(user){
+             return $http.get('/rest/notifications/unreadamount/'+user)
+                       .then(function(result) {
+                            //resolve the promise as the data
+                            return result.data;
+                        });
+        }
+    services.getNotifications = function(user){
+        //return the promise directly.
+             return $http.get('/rest/notifications/'+user)
+                       .then(function(result) {
+                            //resolve the promise as the data
+                            return result.data;
+                        });
+        }
+    return services;
+});
+
+/*var vivServices = angular.module('vivServices',[]);
+ 
+vivServices.factory('services', function($http, $http) {
+	var services = new Object();
+
+    services.getAllWorks = function() {
+             //return the promise directly.
+             return $http.get('/rest/works/all')
+                       .then(function(result) {
+                            //resolve the promise as the data
+                            return result.data;
+                        });
+        }
+
+    services.getUserWorks = function(user) {
+             //return the promise directly.
+             return $http.get('/rest/works/user/'+user)
+                       .then(function(result) {
+                            //resolve the promise as the data
+                            return result.data;
+                        });
+        }
+    services.getWork = function(workId) {
+             //return the promise directly.
+             return $http.get('/rest/works/work/'+workId)
+                       .then(function(result) {
+                            //resolve the promise as the data
+                            return result.data;
+                        });
+        }
+    services.getUnreadNotificationsAmount = function(user){
+             return $http.get('/rest/notifications/unreadamount/'+user)
+                       .then(function(result) {
+                            //resolve the promise as the data
+                            return result.data;
+                        });
+        }
+    services.getNotifications = function(user){
+        //return the promise directly.
+             return $http.get('/rest/notifications/'+user)
+                       .then(function(result) {
+                            //resolve the promise as the data
+                            return result.data;
+                        });
+        }
+    return services;
+});*/
